2e0540bc3e6358f0f7e8f9f677bd4e26526dce60
Damian Agustin Baluja <damian_baluja@hotmail.com>
2026-07-05 18:33:51 -0300

    "API-DOG caída. Agregado JS."

---
+++ js/script.js
@@ -0,0 +1,8 @@
+estado = {
+    raza: ``,
+    fotoa: []
+};
+
+const inputRaza = document.getElementById(`inputRaza`);
+const btnBuscar = document.getElementById(`btnBuscar`);
+const contenedorResultados = document.getElementById(`contenedor-resultados`);
